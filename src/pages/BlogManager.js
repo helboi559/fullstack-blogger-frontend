c08c723cc774342a014df1d91ab5baf32f1e22e2
Juan Ramirez <rami8224.2007@gmail.com>
2022-07-13 15:06:47 -0500

    added css styling

--- src/pages/BlogManager.js
+++ src/pages/BlogManager.js
@@ -3,9 +3,9 @@ import BlogManagerCard from '../components/BlogManagerCard'
 
 const BlogManager = (props) => {
     return (
-    <ul>{props.adminBlogList.map((blog)=> {
+    <ul >{props.adminBlogList.map((blog)=> {
         return (
-            <li key={`blog-id-${blog.id}`}>
+            <li key={`blog-id-${blog.id}`} className={`admin-blog id-${blog.id}`}>
                 <BlogManagerCard blog={blog} deleteBlog={props.deleteBlog}/>
             </li>
         )
